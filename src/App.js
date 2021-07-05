5c30cc12f552ea9c7b88976fb6cd0a05a292ba2d
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-07-05 04:54:42 +0400

    Design the logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import MainPage from './pages/main.page'
+import Logo from './components/logo'
 
 
 export class App extends Component {
   render() {
     return (
       <div className="md:px-24 px-4">
+        <Logo/>
          <Switch>
           <Route exact path="/" component={MainPage}/>
           {/* <Route exact path="/description/:id" component={Description} /> */}
---
+++ src/components/logo.jsx
@@ -0,0 +1,9 @@
+import logo from "../images/logo.png";
+
+const Logo = () => (
+  <div className="my-2 cursor-pointer">
+    <img src={logo} alt="Logo" />
+  </div>
+);
+
+export default Logo;
