d31eb9de3695e0b2437a5414f6b40bdb8e3ae048
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-07-07 08:56:06 +0400

    Create footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import MainPage from './pages/main.page'
-import DescriptionPage from './pages/description.page'
-import Logo from './components/logo'
-
+import MainPage from "./pages/main.page";
+import DescriptionPage from "./pages/description.page";
+import Logo from "./components/logo";
+import Footer from "./components/footer";
 
 export class App extends Component {
   render() {
     return (
-      <div className="md:px-24 px-4">
-        <Logo/>
-         <Switch>
-          <Route exact path="/" component={MainPage}/>
-          <Route exact path="/description/:id" component={DescriptionPage} />
-          {/* <Route component={notFound} /> */}
-        </Switch>
+      <div>
+        <div className="md:px-24 px-4">
+          <Logo />
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/description/:id" component={DescriptionPage} />
+            {/* <Route component={notFound} /> */}
+          </Switch>
+        </div>
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <div className='text-center py-6 mt-12 text-gray-200 bg-gray-700 w-full'>
+    <a href="https://www.mikesargsyan.com/">copyright@mikesargsyan</a>
+  </div>
+);
+
+export default Footer;
